55c3a37d3d9c5c1b2bdf68cf8fe09deec62dd386
YoEugene <dreammacer.yo@gmail.com>
2015-10-12 01:17:47 +0800

    small changes

--- config/config.js
+++ config/config.js
@@ -1,1 +1,3 @@
-module.exports = require('./' + (process.env.NODE_ENV || 'development') + '.json');
+// module.exports = require('./' + (process.env.NODE_ENV || 'development') + '.json');
+
+module.exports = require('./production.json');
